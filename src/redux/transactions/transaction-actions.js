d3ea73ab4b12b4858da984b4ad477c390ea0ee3c
Tetiana1386 <tanyashostak13@gmail.com>
2021-11-13 21:25:48 +0200

    adds transactions to the reducer

---
+++ src/redux/transactions/transaction-actions.js
@@ -0,0 +1,40 @@
+import { createAction } from '@reduxjs/toolkit';
+
+export const fetchTransRequest = createAction(
+    'transaction/fetchTransRequest');
+export const fetchTransSuccess = createAction(
+    'transaction/fetchTransSuccess');
+export const fetchTransError = createAction(
+    'transaction/fetchTransError');
+
+
+export const addTransRequest = createAction(
+    'transaction/addTransRequest');
+export const addTransSuccess = createAction(
+    'transaction/addTransSuccess');
+export const addTransError = createAction(
+    'transaction/addTransError');
+
+
+export const deleteTransRequest = createAction(
+    'transaction/deleteTransRequest');
+export const deleteTransSuccess = createAction(
+    'transaction/deleteTransSuccess');
+export const deleteTransError = createAction(
+    'transaction/deleteTransError');
+
+
+export const updateTransRequest = createAction(
+    'transaction/updateTransRequest');
+export const updateTransSuccess = createAction(
+    'transaction/updateTransSuccess');
+export const updateTransError = createAction(
+    'transaction/updateTransError');
+
+
+export const filterTransRequest = createAction(
+    'transaction/filterTransRequest');
+export const filterTransSuccess = createAction(
+    'transaction/filterTransSuccess');
+export const filterTransError = createAction(
+    'transaction/filterTransError');
---
+++ src/redux/transactions/transaction-operations.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+
+import {
+    fetchTransRequest,
+    fetchTransSuccess,
+    fetchTransError,
+    addTransRequest,
+    addTransSuccess,
+    addTransError,
+    deleteTransRequest,
+    deleteTransSuccess,
+    deleteTransError,
+    updateTransRequest,
+    updateTransSuccess,
+    updateTransError,
+    filterTransRequest,
+    filterTransSuccess,
+    filterTransError
+} from './transaction-actions';
+
+//axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+export const fetchTransactions = () => async dispatch => {
+    dispatch(fetchTransRequest());
+    try {
+        const { data } = await axios.get('/transactions');
+        dispatch(fetchTransSuccess(data));
+    } catch (error) {
+        dispatch(fetchTransError(error.message));
+    }
+};
+
+export const addTransactions = transaction => async dispatch => {
+    dispatch(addTransRequest());
+    try {
+        const { data } = await axios.post('/transactions', transaction);
+        dispatch(addTransSuccess(data));
+    } catch (error) {
+        dispatch(addTransError(error.message));
+    }
+};
+
+export const deleteTransaction = transactionId => async dispatch => {
+    dispatch(deleteTransRequest());
+    try {
+        await axios.delete(`/transactions/${transactionId}`);
+        dispatch(deleteTransSuccess(transactionId));
+    } catch (error) {
+        dispatch(deleteTransError(error.message));
+    }
+};
+
+export const updateTransaction = ({ date, transactionId }) => async dispatch => {
+    dispatch(updateTransRequest());
+    const updateTransaction = { date};
+    try {
+        const { data } = await axios.patch(
+            `/transactions/${transactionId}`,
+            updateTransaction,
+        );
+        dispatch(updateTransSuccess(data));
+    } catch (error) {
+    dispatch(updateTransError(error.message));
+    }
+};
+
+export const filterTransaction = (month, age) => async dispatch => {
+    dispatch(filterTransRequest());
+
+    try {
+        const { data } = await axios.get(`/categories?month=${month}&year=${age}`);
+        dispatch(filterTransSuccess(data));
+    } catch (error) {
+        dispatch(filterTransError(error.message));
+    }
+};
