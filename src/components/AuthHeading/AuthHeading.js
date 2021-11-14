64063bd9e3805bbc5645e02c7842fa33057953b8
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-11-14 16:51:21 +0200

    C:/Program Files/Git/Update size logo-icon/

--- src/components/AuthHeading/AuthHeading.js
+++ src/components/AuthHeading/AuthHeading.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { ReactComponent as IconLogo } from '../icons/icon-logo30.svg';
+import useSizeScreen from '../../hooks/useSizeScreen';
+import { Logo } from '../IconBtn/Logo';
+// import { ReactComponent as IconLogo } from '../icons/icon-logo30.svg';
 
 import styles from './AuthHeading.module.scss';
 
 export default function AuthHeading({ text }) {
+  const sizeScreen = useSizeScreen();
+
   return (
     <div className={styles.head}>
-      <IconLogo width={30} height={30} className={styles.icon} />
+      {/* <IconLogo width={30} height={30} className={styles.icon} /> */}
+
+      {sizeScreen <= 767 ? (
+        <Logo svg={styles.svgLogo} />
+      ) : (
+        <Logo svg={styles.svgLogo} />
+      )}
+
       <h2 className={styles.Heading}>{text}</h2>
     </div>
   );
