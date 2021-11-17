034bd312dc51b4133ddb346c8c93499256289628
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-11-17 18:48:52 +0200

    C:/Program Files/Git/fix/

--- src/components/TransactionForm/TransactionForm.js
+++ src/components/TransactionForm/TransactionForm.js
@@ -1,24 +1,21 @@
 import React, { useState } from 'react';
-import Select from "react-select";
-import style from './TransactionForm.module.scss'; 
-import { Formik, Form, Field, } from 'formik';
-
+import Select from 'react-select';
+import style from './TransactionForm.module.scss';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 import SelectCategoryItem from './SelectCategoryItem';
 import Button from '../Button';
 import Switch from './Switch';
-
 import { costs } from '../../assets/data/select-data/selectData';
 
 import styles from './TransactionForm.module.scss';
 
-
-import DatePicker from "react-datepicker";
-import moment from "moment";
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 //import Box from '@material-ui/core/Box';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 import AddTransaction from '../AddTransactionsButton/AddTransaction';
 //redux
 //import { useDispatch } from 'react-redux';
@@ -47,7 +44,6 @@ export default function TransactionForm() {
     comment: Yup.string(),
   });
 
-
   return (
     <div className={styles.container}>
       <div className={styles.form}>
@@ -67,11 +63,6 @@ export default function TransactionForm() {
           {({ errors, touched, isSubmitting }) => (
             <Form className={styles.Form}>
               {/* <div id="my-radio-group"></div>
-
-
-
-
-  
           <div role="group" aria-labelledby="my-radio-group">
             <label>
               <Field type="radio" name="typeOfTransaction" value="Доход" />
@@ -86,7 +77,6 @@ export default function TransactionForm() {
               )}
           </div> */}
 
-
               <div className={styles.box}>
                 <p
                   className={styles.text}
@@ -94,7 +84,6 @@ export default function TransactionForm() {
                 >
                   Income
                 </p>
-
                 <Switch
                   onSwitch={chooseSelect => onSwitchChecked(chooseSelect)}
                   isChecked={chooseSelect}
@@ -173,103 +162,7 @@ export default function TransactionForm() {
             </Form>
           )}
         </Formik>
-
-              
-           
-//        <div className={style.box}>  
-//            <p className={style.text} style={{ color: 'rgba(36, 204, 167, 1)' }}>
-//             Income
-//           </p> 
-             
-//             <Switch    
-//             onSwitch={chooseSelect => onSwitchChecked(chooseSelect)}
-//             isChecked={chooseSelect}
-//             onClick={chooseSelect => onSwitchChecked(chooseSelect)}
-//           />
-//           <p className={style.text} style={{ color: 'rgba(255, 101, 150, 1)' }}>
-//             Cost
-//           </p>
-           
-//          </div> 
-      
-            
-//             {/* <Field name="category" as="select" hidden>
-//               <option value="">Choose category</option>
-//               <option value="Основной">Основной</option>
-//               <option value="Еда">Еда</option>
-//               <option value="Авто">Авто</option>
-//               <option value="Развитие">Развитие</option>
-//               <option value="Дети">Дети</option>
-//               <option value="Дом">Дом</option>
-//               <option value="Образование">Образование</option>
-//               <option value="Остальное">Остальное</option>
-//             </Field> */}
-              
-// <div className={style.select}>
-//               <Select
-//                  className="select"
-//                 classNamePrefix="selectprefix"
-//                 isIncome={!chooseSelect}
-//                 category={category}
-//                 placeholder="Choose category"
-//                 isSearchable={true}
-//                 name="category"
-//                 value={category}
-//                 onChange={handleSelect}
-//               />
-// </div>
-
-//             {errors.category && touched.category &&
-//         <span className="input-feedback">
-//           {errors.category}
-//         </span>}
-// <div className={style.datebform}>
-//       <Field
-//             className={style.inputNumber}
-//             name="amount"
-//             type="number"
-//             placeholder="0.00"
-//             //value={amount}
-//             //onChange={updateAnmount}
-//              />
-//         {errors.amount &&
-//         touched.amount &&
-//         <span className="input-feedback">
-//           {errors.amount}
-//         </span>}
-            
-//          {/*    <Field
-//             name="date"
-//             type="date"
-//           /> */}
-              
-//               <DatePicker
-//               id="select"
-//               className={style.calendar}
-//               selected={startDate}
-//               onChange={handleDate}
-//               dateFormat="dd.MM.yyyy"
-//             />
-//    </div>         
-//               <Field
-//                 className={style.textarea}
-//                 name="comment" as="textarea"
-//             type="text"
-//             placeholder="Comment" />
-
-//         <Button  disabled={isSubmitting} type="submit" contentBtn="Add" />
-//         {/* <Button className={style.btn2}  type="submit" contentBtn="Cancel" /> */}
-// <button className={style.btn2} >
-//             Cancel
-//           </button>
-//           {/* {isLoading && <LoaderSpinner />} */}
-//         </Form>
-//       )}
-//       </Formik>
-    
-
       </div>
-      
     </div>
   );
 }
