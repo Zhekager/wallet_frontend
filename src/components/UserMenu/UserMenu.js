a1c484895b57b4890a260175ff85f3d17fb60147
sonya287 <starodyb1987@gmail.com>
2021-11-17 16:00:18 +0200

    1234

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -3,9 +3,8 @@ import { getUserEmail, logOut } from '../../redux/auth';
 
 import { button, user } from './UserMenu.module.scss';
 
-const UserMenu = ({ avatar, email, onLogout }) => (
+const UserMenu = ({ email, onLogout }) => (
   <div>
-    <img src={avatar} alt="" width="32" />
     <span>
       Вітаємо, <span className={user}>{email}</span>{' '}
     </span>
