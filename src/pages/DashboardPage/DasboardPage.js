255cd355a38bd93a4201fa67ee7b367c983c0f46
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-11-18 11:00:10 +0200

    adds modal into DashboargPage

--- src/pages/DashboardPage/DasboardPage.js
+++ src/pages/DashboardPage/DasboardPage.js
@@ -1,17 +1,33 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import HomeTab from '../../components/HomeTab';
+import ModalAddTransaction from '../../components/ModalAddTransaction';
+import TransactionForm from '../../components/TransactionForm';
+import ButtonIcon from '../../components/ModalAddTransaction/ButtonIcon';
+import Button from '../../components/Button';
+import { HiX } from 'react-icons/hi';
 // import Currency from '../../components/Sidebar/Currency';
-import './DashboardPage.scss';
+
+import styles from './DashboardPage.scss';
 
 export default function DashboardPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const onOpenModal = e => {
+    toggleModal();
+  };
+
   return (
     <>
       <Header />
-      <div className="dashboard">
-        <div className="container">
+      <div className={styles.dashboard}>
+        <div className={styles.container}>
           <Sidebar />
           <main>
             <Switch>
@@ -19,6 +35,28 @@ export default function DashboardPage() {
               <Route />
               {/* <Route path="/currency" component={Currency} /> */}
             </Switch>
+
+            <ButtonIcon
+              onClick={onOpenModal}
+              aria-label="Open modal"
+              btnClass="ButtonIconAdd"
+            >
+              +
+            </ButtonIcon>
+
+            {showModal && (
+              <ModalAddTransaction onClose={toggleModal}>
+                <TransactionForm onClose={toggleModal} />
+
+                <ButtonIcon
+                  btnClass="ButtonIconClose"
+                  onClick={toggleModal}
+                  aria-label="Close modal"
+                >
+                  <HiX />
+                </ButtonIcon>
+              </ModalAddTransaction>
+            )}
           </main>
         </div>
       </div>
