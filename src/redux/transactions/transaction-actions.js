3ebf5ac6250fb16bf3955462694ec10695083797
Tetiana1386 <tanyashostak13@gmail.com>
2021-11-24 12:31:23 +0200

    adds transactions/auth

--- src/redux/transactions/transaction-actions.js
+++ src/redux/transactions/transaction-actions.js
@@ -4,9 +4,9 @@ export const fetchTransRequest = createAction('transaction/fetchTransRequest');
 export const fetchTransSuccess = createAction('transaction/fetchTransSuccess');
 export const fetchTransError = createAction('transaction/fetchTransError');
 
-export const addTransRequest = createAction('transaction/addTransRequest');
-export const addTransSuccess = createAction('transaction/addTransSuccess');
-export const addTransError = createAction('transaction/addTransError');
+// export const addTransRequest = createAction('transaction/addTransRequest');
+// export const addTransSuccess = createAction('transaction/addTransSuccess');
+// export const addTransError = createAction('transaction/addTransError');
 
 export const filterTransRequest = createAction(
   'transaction/filterTransRequest',
